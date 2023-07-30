6dac15757ca63f5e31ff74f06fa78e1e3126215c
YuliaSilk <129064716+YuliaSilk@users.noreply.github.com>
2023-07-30 17:17:41 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const elements = {
+    form:document.querySelector(".login-form")
+};
+elements.form.addEventListener('submit', handlerSubmit);
+function handlerSubmit(evt) {
+    evt.preventDefault();
+    // console.dir(evt.currentTarget);
+    const {email, password} = evt.currentTarget.elements;
+    if (email.value === "" || password.value === "" ) {
+        alert('Будь ласка, заповніть всі поля');
+        elements.form.reset();
+    }else{
+        console.log({Email: email.value,
+            Password: password.value})
+    }
+    elements.form.reset()
+     }
 
