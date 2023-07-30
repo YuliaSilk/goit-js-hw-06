fb7506ccd1dbd9386350c2ee0452003328df76dd
YuliaSilk <129064716+YuliaSilk@users.noreply.github.com>
2023-07-30 12:33:28 +0300

    Update task-07.js

---
+++ js/task-07.js
@@ -0,0 +1,8 @@
+const elements = {
+    fontSizeControler: document.querySelector('#font-size-control'),
+    text: document.querySelector('#text')
+};
+elements.fontSizeControler.addEventListener('input', handlerChange);
+function handlerChange(evt) {
+    text.style.fontSize = `${evt.currentTarget.value}px`;
+};
