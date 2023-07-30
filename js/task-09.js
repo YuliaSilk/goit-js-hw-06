080f6c2811cbcbe7dba5d6b4ea0215c35c84a4c0
YuliaSilk <129064716+YuliaSilk@users.noreply.github.com>
2023-07-30 16:29:37 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const body = document.querySelector('body');
+const button = document.querySelector('.change-color');
+const spanColorValue = document.querySelector('.color');
+
+button.addEventListener('click', onClick);
+function onClick(evt) {
+  let randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  spanColorValue.textContent = randomColor;
+
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
